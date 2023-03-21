289904a960c158aecf95d8e2cc02c3e89021d624
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 01:56:41 +0200

    gg

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net'],
   },
-  experimental: {
-    appDir: true
-  },
+  //experimental: {
+  //  appDir: true
+  //},
 }
