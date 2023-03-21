a88c4ba1b8df6a56593bd3b0a317339a45032180
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 01:06:21 +0200

    test

--- app/(site)/Navbar.jsx
+++ app/(site)/Navbar.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
 
-
-import Link from 'next/link'
 import { NavLinks } from '@/lib/NavList'
 
 export default function Navbar() {
@@ -14,32 +12,7 @@ export default function Navbar() {
 
 
     return (
-        <>
-            <nav className={pos ? 'Navbar active' : 'Navbar'}>
-                <div className="top">
-                    <div className="logo">
-                        <Link href="/">
-                            <span>Commerce Students</span>
-                        </Link>
-                    </div>
-                    <div className="tiggers">
-                        {/* <FontAwesomeIcon icon={faSearch} className="icon" />
-                        <FontAwesomeIcon onClick={darkMode} icon={click ? faSun : faMoon} className="icon" />
-                        <FontAwesomeIcon onClick={handelClick} icon={pos ? faTimes : faBars} className={pos ? 'tigger active' : 'tigger'} /> */}
-                    </div>
-
-                </div>
-
-                <div className={pos ? 'navContent active' : 'navContent'}>
-
-                    {NavLinks.map((link) => (
-                        <Link href={link.link}>
-                            <a key={link.id}>{link.title}</a>
-                        </Link>
-                    ))}
-                    <Link href="/"><a className="login">Login</a></Link>
-                </div>
-            </nav>
-        </>
+        <nav>
+        </nav>
     )
 }
