5c440c13416455b039ff9d461bd4a123d1c5500a
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 01:37:54 +0200

    tart

--- app/(site)/Navbar.jsx
+++ app/(site)/Navbar.jsx
@@ -3,10 +3,7 @@ import { useState, useEffect } from 'react'
 import { NavLinks } from '@/lib/NavList'
 
 export default function Navbar() {
-    const [dark, setDark] = useState(undefined)
     const [click, setClick] = useState(false);
-    const [pos, setPos] = useState(false);
-
 
     const handelClick = () => setPos(!pos)
 
--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,14 +1,12 @@
 import './globals.css'
 import Footer from './Footer'
 import Navbar from './Header'
-import Head from "./head"
 
 export default function RootLayout({
     children,
 }) {
     return (
         <html lang="ar">
-            <Head />
             <body>
                 <Navbar />
                     <main className="main">{children}</main>
