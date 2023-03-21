7a469cabe0e449f646ee5dbdc9401d4e48f9147c
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 01:50:50 +0200

    start

--- app/Footer.jsx
+++
@@ -1,8 +0,0 @@
- export default function Footer() {
-    return (
-        <footer className="footer">
-            <h4>Support Us in <a>Paypal</a></h4>
-            <p>Copyright {new Date().getFullYear()} by Student Commerce</p>
-        </footer>
-    )
-}
