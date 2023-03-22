74cb38e4893136f5cb51337ca284a6d032e406c8
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 16:43:25 +0200

    icons

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,9 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+// import NavList from '../Navbar/NavMenu/NavList'
+// import SearchBox from '../Navbar/NavMenu/SearchBox';
+
+import { RiSearch2Line, RiMenu3Line } from 'react-icons/ri'
+import { useState } from 'react';
+
 export default function Navbar() {
+    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
+    const [btn, setBtn] = useState(false);
+    const [searchOpen, setSearchOpen] = useState(false);
+
     return (
         <nav>
-            <h1>
-                مكتبة قومية
-            </h1>
+            <RiMenu3Line />
+            <Link href={"/"}>
+                <h1>
+                    المكتبة القومية
+                </h1>
+            </Link>
+            <RiMenu3Line />
         </nav>
     )
 }
