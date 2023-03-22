44afcf245ffc9cdcaa8ddee3179b76ffcb4bf53a
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-23 00:40:52 +0200

    seo links

--- Components/Navbar/NavList.jsx
+++ Components/Navbar/NavList.jsx
@@ -9,30 +9,31 @@ export default function NavList({ btn, setBtn }) {
   return (
     <>
       <div className={`navList ${btn ? `right-0` : `right-[-10000px]`}`}>
-          <div className="flex justify-between items-center">
-            <RiCloseLine
-              className="rounded-full text-xl p-3 w-[3em] h-[3em] mb-8 transition-all duration-300 cursor-pointer hover:bg-white hover:text-primary"
-              onClick={() => setBtn(!btn)}
-            />
-          </div>
+        <div className="flex justify-between items-center">
+          <RiCloseLine
+            className="rounded-full text-xl p-3 w-[3em] h-[3em] mb-8 transition-all duration-300 cursor-pointer hover:bg-white hover:text-primary"
+            onClick={() => setBtn(!btn)}
+          />
+        </div>
 
-          <ul className="px-0">
-            {NavLinks.map((link) => (
-              <Link href={link.link}>
+        <ul className="px-0">
+          {NavLinks.map((link) => (
+            <Link href={link.link}>
 
-                <li
-                  className="navLink"
-                  key={link.id}
-                  onClick={() => setBtn(!btn)}
+              <li
+                className="navLink"
+                key={link.id}
+                onClick={() => setBtn(!btn)}
+              >
+                <a href={siteUrl + link.link} aria-label={link.title}
                 >
-                  <a href={siteUrl + link.link} aria-label={link.title}
-                  >
-                    {link.title}
-                  </a>
-                </li>
-              </Link>
-            ))}
-          </ul>
+                  {link.title}
+                </a>
+
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </>
   );
