24c973b06b49aa55fae0c333e393dc138319c99f
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-22 17:59:15 +0200

    footer link

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,11 +1,15 @@
+import FooterLinks from "@lib/FooterLink"
+
 export default function Footer() {
     return (
         <footer>
             فورتر
 
-            {<ul>
-                <li></li>
-            </ul>}
+            {FooterLinks.map((link) => (
+            <ul key={link.id} className="flex gap-5 items-center w-full">
+                <li>{link.title}</li>
+            </ul>
+            ))}
         </footer>
     )
 }
