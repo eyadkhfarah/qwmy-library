266da972c9844db54116ad111499434438f1578b
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-23 02:25:43 +0200

    styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 export default function Home() {
     return (
         <>
-        <div className="text-center">
-        <h1>ابحث في المكتبة القومية</h1>
-       <p>بوابتك المعرفية في عالم القومية</p>
+            <div className="text-center h-screen grid items-center">
+                <div className="grid gap-3">
+                    <h1>ابحث في المكتبة القومية</h1>
+                    <p>بوابتك المعرفية في عالم القومية</p>
 
-       <input className="searchInput" type="text" placeholder="ابحث في عالم القومية"/>
-
-        </div>
+                    <input className="searchInput" type="text" placeholder="ابحث في عالم القومية" />
+                </div>
+            </div>
         </>
     )
 }
