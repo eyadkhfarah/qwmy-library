a551614767cb35c8e1b867e0c6049cfb8e30e0bc
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-24 00:26:48 +0200

    flex

--- Components/Navbar/index.jsx
+++ Components/Navbar/index.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
 
                 <div className="navIcon">{Search}</div>
 
-                <ul className="px-0 md:block hidden flex gap-4 items-center ">
+                <ul className="px-0 hidden md:flex gap-4 items-center ">
                     {NavLinks.map((link) => (
                         <Link href={link.link}>
                             <li
