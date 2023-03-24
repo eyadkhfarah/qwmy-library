2eefab52b4c6e6302c4a48a3aaf8c02583a5eebb
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-24 03:55:09 +0200

    contrubtion

--- pages/contributors.jsx
+++ pages/contributors.jsx
@@ -1,6 +1,33 @@
-export default function Contrub() {
+import { NextSeo } from 'next-seo';
+import Link from "next/link";
+
+export default function Contrib() {
+    const title = "المكتبة القومية — المساهمين"
+    const desc = "قائمة المساهمين في الموقع بشكل مادي، كن انت من المساهمين."
+    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
     return (
         <>
+            <NextSeo
+                title={title}
+                description={desc}
+                openGraph={{
+                    title: `${title}`,
+                    description: `${desc}`,
+                    type: 'website',
+                }}
+                additionalMetaTags={[
+                    {
+                        itemProp: 'name',
+                        content: { title }
+                    },
+                    {
+                        itemProp: 'description',
+                        content: { desc }
+                    },
+                ]}
+            />
+
             <h1>مساهمينا</h1>
         </>
     )
