4d12ab84043df8da60a94fd9d1e3dd265ece5f85
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-25 17:05:36 +0200

    text gray

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -4,12 +4,12 @@ export default function Books() {
     return (
         <>
             <h1>كتب عن القومية</h1>
-            <div className="gird gap-3 p-4 my-4 border-b-1">
+            <div className="gird gap-3 p-4 my-4 border-b-2">
                 <div className="flex items-center gap-4">
                     <RiArrowLeftSLine className="text-2xl" />
                     <a href="/"><h2 className="border-none">كتاب معين</h2></a>
                 </div>
-                <div className="flex gap-5">
+                <div className="flex text-gray-500 gap-5">
                     <p>نوع الكتاب</p>
                     <p>اسم المؤلف</p>
                 </div>
