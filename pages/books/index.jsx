0635dd78774b7a98a8752bbfa6e903a86fd6af88
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-25 18:52:39 +0200

    console

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -29,7 +29,6 @@ export default function Books({ books }) {
     const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
     console.log(books.fields)
-
     return (
         <>
             <NextSeo
