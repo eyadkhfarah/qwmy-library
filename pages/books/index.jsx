20fafd0b0975558f507e829a2c2766fb37790bdd
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-25 16:54:38 +0200

    add dumy data

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { RiArrowLeftSLine, RiSearchLine } from "react-icons/ri";
 
 export default function Books() {
-  return (
-    <>
-    <h1>كتب عن القومية</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>كتب عن القومية</h1>
+            <div className="gird gap-3 my-">
+                <div className="flex gap-4">
+                    <RiArrowLeftSLine className="2xl" />
+                    <h2 className="border-none">كتاب معين</h2>
+                </div>
+                <div className="flex gap-5">
+                    <p>نوع الكتاب</p>
+                    <p>اسم المؤلف</p>
+                </div>
+            </div>
+        </>
+    )
 }
