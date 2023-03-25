f028ec7c7bcb9eb9934a60eb5bec9f56663a79bd
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-25 17:14:17 +0200

    seo books

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -1,8 +1,38 @@
 import { RiArrowLeftSLine, RiSearchLine } from "react-icons/ri";
+import { createClient } from 'contentful';
+
+import { NextSeo } from 'next-seo';
+import Link from "next/link";
+
+import ReactPaginate from 'react-paginate';
 
 export default function Books() {
+    const title = "المكتبة القومية — كتب عن القومية"
+    const desc = "اكتشف مجموعة كبيرة من الكتب التي تتحدث عن القومية."
+    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
     return (
         <>
+            <NextSeo
+                title={title}
+                description={desc}
+                openGraph={{
+                    title: `${title}`,
+                    description: `${desc}`,
+                    type: 'website',
+                }}
+                additionalMetaTags={[
+                    {
+                        itemProp: 'name',
+                        content: { title }
+                    },
+                    {
+                        itemProp: 'description',
+                        content: { desc }
+                    },
+                ]}
+            />
+
             <h1>كتب عن القومية</h1>
             <div className="gird gap-3 p-4 my-4 border-b-2">
                 <div className="flex items-center gap-4">
