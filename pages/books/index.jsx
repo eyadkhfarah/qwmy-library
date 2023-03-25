eef508920349cd2b08b4ae224af422c81c2d8067
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-03-25 17:24:04 +0200

    add contemt

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -6,6 +6,23 @@ import Link from "next/link";
 
 import ReactPaginate from 'react-paginate';
 
+export async function getStaticProps() {
+
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    })
+  
+    const res = await client.getEntries({ content_type: "books" })
+    
+    return {
+      props: {
+        books: res.items,
+      },
+      revalidate: 1
+    }
+  }
+
 export default function Books() {
     const title = "المكتبة القومية — كتب عن القومية"
     const desc = "اكتشف مجموعة كبيرة من الكتب التي تتحدث عن القومية."
@@ -37,7 +54,7 @@ export default function Books() {
             <div className="gird gap-3 p-4 my-4 border-b-2">
                 <div className="flex items-center gap-4">
                     <RiArrowLeftSLine className="text-2xl" />
-                    <a href="/"><h2 className="border-none">كتاب معين</h2></a>
+                    <a href="/"><h2 className="border-none text-xl">كتاب معين</h2></a>
                 </div>
                 <div className="flex text-gray-500 gap-5">
                     <p>نوع الكتاب</p>
