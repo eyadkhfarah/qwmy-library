52b9241779c9909c54d6f6bd7586d928b29e7953
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-04-11 23:56:30 +0200

    move search bar

--- Components/Navbar/index.jsx
+++ Components/Navbar/index.jsx
@@ -1,81 +1,115 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
-import NavList from './NavList'
+import NavList from "./NavList";
 import { NavLinks } from "@lib/NavList";
-import SearchList from './SearchList';
+import SearchList from "./SearchList";
 
 import { useRouter } from "next/router";
 
 import { useTheme } from "next-themes";
-import { RiSearch2Line, RiMenu3Line } from 'react-icons/ri'
-import { useState } from 'react';
+import { RiSearch2Line, RiMenu3Line } from "react-icons/ri";
+import { useState } from "react";
 
 export default function Navbar() {
-    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+  const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
-    const [btn, setBtn] = useState(false);
-    const [searchOpen, setSearchOpen] = useState(false);
+  const [btn, setBtn] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
 
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === "system" ? systemTheme : theme;
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const Search = <span><RiSearch2Line /></span>;
-    const Menu = <span><RiMenu3Line /></span>;
+  const Search = (
+    <span>
+      <RiSearch2Line />
+    </span>
+  );
+  const Menu = (
+    <span>
+      <RiMenu3Line />
+    </span>
+  );
 
-    return (
-        <>
-            <nav className={`${router.pathname === "/" ? "shadow-none dark:bg-none" : ""}`}>
-                <div onClick={() => setBtn(!btn)} className="navIcon md:hidden block">{Menu}</div>
+  return (
+    <>
+      <nav
+        className={`${
+          router.pathname === "/" ? "shadow-none dark:bg-none" : ""
+        }`}
+      >
+        <div onClick={() => setBtn(!btn)} className="navIcon md:hidden block">
+          {Menu}
+        </div>
 
-                <div className={`${router.pathname === "/" ? "hidden" : "block"}`}>
-                    <Link href={"/"}>
-                        <a href={siteUrl}>
-                            {currentTheme === "dark" ?
-                                <Image src="/WhiteLogo.svg" alt="لوجو المكتبة القومية" aria-label="لوجو المكتبة القومية" width={120} height={47} />
-                                :
-                                <Image src="/DarkLogo.svg" alt="لوجو المكتبة القومية" aria-label="لوجو المكتبة القومية" width={120} height={47} />
-                            }
-                        </a>
-                    </Link>
-                </div>
+        <div className="flex gap-5">
+          <div className={`${router.pathname === "/" ? "hidden" : "block"}`}>
+            <Link href={"/"}>
+              <a href={siteUrl}>
+                {currentTheme === "dark" ? (
+                  <Image
+                    src="/WhiteLogo.svg"
+                    alt="لوجو المكتبة القومية"
+                    aria-label="لوجو المكتبة القومية"
+                    width={120}
+                    height={47}
+                  />
+                ) : (
+                  <Image
+                    src="/DarkLogo.svg"
+                    alt="لوجو المكتبة القومية"
+                    aria-label="لوجو المكتبة القومية"
+                    width={120}
+                    height={47}
+                  />
+                )}
+              </a>
+            </Link>
+          </div>
 
-                <div onClick={() => setSearchOpen(!searchOpen)} className="navIcon md:hidden block">{Search}</div>
+          <div className="md:flex hidden gap-2 p-1 border dark:border-none dark:bg-dlight">
+            <div className="navIcon">{Search}</div>
 
-                <div className="md:flex hidden gap-2 p-1 border dark:border-none dark:bg-dlight">
-                    <div className="navIcon">{Search}</div>
+            <input
+              // value={}
+              // onChange={}
+              className="searchInput border-none"
+              type="text"
+              placeholder="ابحث في عالم القومية"
+            />
+          </div>
+        </div>
+        <div
+          onClick={() => setSearchOpen(!searchOpen)}
+          className="navIcon md:hidden block"
+        >
+          {Search}
+        </div>
 
-                    <input
-                        // value={}
-                        // onChange={}
-                        className="searchInput border-none"
-                        type="text"
-                        placeholder="ابحث في عالم القومية" />
-                </div>
-
-
-                <ul className="hidden md:flex text-[.7rem] gap-3 items-center ">
-                    {NavLinks.map((link) => (
-                        <Link href={link.link}>
-                            <li
-                                className="navLink"
-                                key={link.id}
-                                onClick={() => setBtn(!btn)}
-                            >
-                                <a href={siteUrl + link.link} aria-label={link.title}
-                                >
-                                    {link.title}
-                                </a>
-                            </li>
-                        </Link>
-                    ))}
-                </ul>
-
-            </nav>
-            <SearchList searchOpen={searchOpen} setSearchOpen={setSearchOpen} className="md:hidden" />
-            <NavList btn={btn} setBtn={setBtn} className="md:hidden" />
-        </>
-    )
+        <ul className="hidden md:flex text-[.7rem] gap-3 items-center ">
+          {NavLinks.map((link) => (
+            <Link href={link.link}>
+              <li
+                className="navLink"
+                key={link.id}
+                onClick={() => setBtn(!btn)}
+              >
+                <a href={siteUrl + link.link} aria-label={link.title}>
+                  {link.title}
+                </a>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <SearchList
+        searchOpen={searchOpen}
+        setSearchOpen={setSearchOpen}
+        className="md:hidden"
+      />
+      <NavList btn={btn} setBtn={setBtn} className="md:hidden" />
+    </>
+  );
 }
