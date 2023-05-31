774ce472a636bb47e514cd4cbe28d73cac853418
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-06-01 01:52:29 +0300

    seo

--- pages/images/[slug].jsx
+++ pages/images/[slug].jsx
@@ -95,15 +95,15 @@ const options = {
 
 
 export default function ImageDetials({ images }) {
-    // const title = images.fields.title + " — المكتبة القومية"
-    // const desc = images.fields.details.content[0].content[0].value;
-    // const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+    const title = images.fields.title + " — المكتبة القومية"
+    const desc = images.fields.details.content[0].content[0].value;
+    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
     if (!images) return <div>تحميل</div>;
 
     return (
         <>
-            {/* <NextSeo
+            <NextSeo
                 title={title}
                 description={desc}
                 openGraph={{
@@ -129,6 +129,8 @@ export default function ImageDetials({ images }) {
                     },
                 ]}
             />
+
+            {/*
             <article className="grid md:grid-cols-2 grid-cols-1 gap-6">
                 <div className="">
                     <h1>{images.fields.title}</h1>
