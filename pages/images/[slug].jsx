37f10a826694b449e77f52efe21eeee05c9fcac2
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-06-01 22:22:58 +0300

    test 2

--- pages/images/[slug].jsx
+++ pages/images/[slug].jsx
@@ -97,7 +97,7 @@ const options = {
 export default function ImageDetials({ images }) {
     // const title = images.fields.title + " — المكتبة القومية"
     // const desc = images.fields.details.content[0].content[0].value;
-    // const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+    const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
     if (!images) return <div>تحميل</div>;
 
@@ -130,9 +130,9 @@ export default function ImageDetials({ images }) {
                 ]}
             />
 
-            {/* <article className="grid md:grid-cols-2 grid-cols-1 gap-6">
+            <article className="grid md:grid-cols-2 grid-cols-1 gap-6">
                 <div className="">
-                    <h1>{images.fields.title}</h1>
+                    {/*       <h1>{images.fields.title}</h1>
                     <div className="my-7 grid gap-3 w-full h-fit md:hidden">
                         <Image priority={true} src={'https:' + images.fields.image.fields.file.url} alt={images.fields.alt} width={images.fields.image.fields.file.details.image.width} height={images.fields.image.fields.file.details.image.height} className="h-fit w-full lg:hidden block" />
                         <span className="font-medium text-gray-500 text-xs">{images.fields.alt}</span>
@@ -183,8 +183,9 @@ export default function ImageDetials({ images }) {
                 <div className="gap-3 w-full h-fit hidden md:grid">
                     <Image priority={true} src={'https:' + images.fields.image.fields.file.url} alt={images.fields.alt} width={images.fields.image.fields.file.details.image.width} height={images.fields.image.fields.file.details.image.height} className="h-fit w-full" />
                     <span className="font-medium text-gray-500 text-xs">{images.fields.alt}</span>
+           */}
                 </div>
-            </article> */}
+            </article>
 
             <div>
                 <h3>صور متعلقة</h3>
