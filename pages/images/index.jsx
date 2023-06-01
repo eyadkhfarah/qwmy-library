3a70e2cbfb3f8fa82f54e8dc3d03e9611ec25373
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-06-02 00:59:07 +0300

    add pagenation

--- pages/images/index.jsx
+++ pages/images/index.jsx
@@ -31,6 +31,19 @@ export default function Images({ images }) {
     const desc = "شاهد اهم واجمل الصور هنا.";
     const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
+    const [imagesPost, setImagesPost] = useState(images);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [ImagesPerPage] = useState(20);
+
+    const indexOfLastPost = currentPage * ImagesPerPage;
+    const indexOfFirstPost = indexOfLastPost - ImagesPerPage;
+    const currentImages = images.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = ({ selected }) => {
+        setCurrentPage(selected + 1);
+    };
+
     return (
         <>
             <NextSeo
@@ -62,21 +75,43 @@ export default function Images({ images }) {
 
 
             <h1>ألبوم صور</h1>
+            {imagesPost ? 
+                <>
+                    <div className="w-full max-w-full py-5 mx-auto mb-10 gap-5 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+                        {images.map((image) => (
+                            <Link href={"/images/" + image.fields.slug} key={image.sys.id}>
+                                <div className="group relative h-fit">
+                                    <a href={"/images/" + image.fields.slug}>
+                                        <Image layout="responsive" objectFit="cover" src={'https:' + image.fields.image.fields.file.url} alt={image.fields.alt} width={image.fields.image.fields.file.details.image.width} height={image.fields.image.fields.file.details.image.height} className="block h-full w-full object-cover object-center group-hover:opacity-50 transition-all ease-in-out duration-300" />
+                                        <span className="absolute bottom-3 right-3 transition-opacity opacity-0 group-hover:opacity-100 text-black text-xl font-black dark:text-white">{image.fields.title}</span>
+                                    </a>
+                                </div>
+                            </Link>
 
-            <div className="w-full max-w-full py-5 mx-auto mb-10 gap-5 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-                {images.map((image) => (
-                    <Link href={"/images/" + image.fields.slug} key={image.sys.id}>
-                        <div className="group relative h-fit">
-                            <a href={"/images/" + image.fields.slug}>
-                                <Image layout="responsive" objectFit="cover" src={'https:' + image.fields.image.fields.file.url} alt={image.fields.alt} width={image.fields.image.fields.file.details.image.width} height={image.fields.image.fields.file.details.image.height} className="block h-full w-full object-cover object-center group-hover:opacity-50 transition-all ease-in-out duration-300" />
-                                <span className="absolute bottom-3 right-3 transition-opacity opacity-0 group-hover:opacity-100 text-black text-xl font-black">{image.fields.title}</span>
-                            </a>
-                        </div>
-                    </Link>
+                        ))}
 
-                ))}
+                    </div>
 
-            </div>
+                    <ReactPaginate
+                        onPageChange={paginate}
+                        breakLabel={"..."}
+                        renderOnZeroPageCount={null}
+                        pageCount={Math.ceil(imagesPost.length / ImagesPerPage)}
+                        previousLabel={'السابق'}
+                        nextLabel={'التالي'}
+
+                        breakClassName={"pageNum"}
+                        breakLinkClassName={"pageNum"}
+                        containerClassName={'pagination'}
+
+                        pageClassName={"pageNu"}
+                        pageLinkClassName={'pageNum'}
+                        previousLinkClassName={'pageSwitch'}
+                        nextLinkClassName={'pageSwitch'}
+                        activeLinkClassName={'pageNumAct'}
+                    />
+                </> : <div>جاري التحميل</div>
+            }
         </>
     )
 }
